d9fc32aac399c74af1af93243229c7c651c28e8c
Roy <rl4940@gmail.com>
2018-10-19 12:53:37 +0100

    some changes

--- items.py
+++ items.py
@@ -1,29 +1,107 @@
 #Python3 items.py
 
 maracas = {'name' : 'Maracas',
+           'id' : 'maracas',
+           'equippable' : True,
+           'description' : """ """
 }
+
 dragons_breath_spell = {'name' : 'Dragons Breath Spell',
+                        'id' : 'spell',
+                        'equippable' : True,
+                        'description' : """ """
 }
+
 bone_saw = {'name' : 'Bone Saw',
+            'id' : 'saw',
+            'equippable' : True,
+            'description' : """ """
 }
+
 bee_stingers = {'name' : 'Bee Stingers',
+                'id' : 'stingers',
+                'equippable' : True,
+                'description' : """ """
 }
+
 workout_routine = {'name' : 'Workout Routine',
+                   'id' : 'routine',
+                   'equippable' : True,
+                   'description' : """ """,
                    'power' : 5
 }
+
 unicorn_spear = {'name' : 'Unicorn Spear',
+                 'id' : 'spear',
+                 'equippable' : True,
+                 'description' : """ """
 }
+
 giraffe_drumstick = {'name' : 'Giraffe Drumstick',
+                     'id' : 'drumstick',
+                     'equippable' : True,
+                     'description' : """ """
 }
+
 nuclear_claws = {'name' : 'Nuclear Claws',
+                 'id' : 'claws',
+                 'equippable' : True,
+                 'description' : """ """
 }
+
 ddos_attack_script = {'name' : 'DDOS Attack Script',
+                      'id' : 'script',
+                      'equippable' : True,
+                      'description' : """ """
 }
+
 holy_water = {'name' : 'Holy Water',
+              'id' : 'water',
+              'equippable' : True,
+              'description' : """ """
 }
+
 broken_bottle = {'name' : 'Broken Bottle',
+                 'id' : 'bottle',
+                 'equippable' : True,
+                 'description' : """ """
 }
+
 trident = {'name' : 'Trident',
+           'id' : 'trident',
+           'equippable' : True,
+           'description' : """ """
 }
 trophy_for_winning_the_game = {'name' : 'Trophy for Winning the Game',
+                               'id' : 'trophy',
+                               'equippable' : False,
+                               'description' : """ """
+}
+
+stick = {'name' : 'Stick',
+         'id' : 'stick',
+         'equippable' : True,
+         'description' : """""",
+         'weapon' : True,
+         'power' : 0
+}
+
+
+
+
+items = {'maracas' : maracas,
+         'spell' : dragons_breath_spell,
+         'saw' : bone_saw,
+         'stingers' : bee_stingers,
+         'routine' : workout_routine,
+         'spear' : workout_routine,
+         'drumstick' : giraffe_drumstick,
+         'claws' : nuclear_claws,
+         'script' : ddos_attack_script,
+         'water' : holy_water,
+         'bottle' : broken_bottle,
+         'trident' : trident,
+         'trophy' : trophy_for_winning_the_game,
+         'stick' : stick
+         
 }
