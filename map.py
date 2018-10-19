30afe385178334269ca07c70782484d85d02583a
Luca Passariello <=>
2018-10-19 13:52:08 +0100

    Fixed dead ends

--- map.py
+++ map.py
@@ -2,67 +2,67 @@
 r1 = {'name' : 'room 1',
       'position' : 'position1',
       'description' :""" """,
-      'exits' : {'north' : 'rm5'} 
+      'exits' : {'north' : 'rm5'}
 }
 
 r2 = {'name' : 'room 2',
       'position' : 'position2',
       'description' :""" """,
-      'exits' : {'north' : 'rm6', 'east' : 'rm3', 'west' : 'rm1'} 
+      'exits' : {'north' : 'rm6', 'east' : 'rm3', 'west' : 'rm1'}
 }
 
 r3 = {'name' : 'room 3',
       'position' : 'position3',
       'description' :""" """,
-      'exits' : {}
+      'exits' : {'west': 'rm2'}
 }
 
 r4 = {'name' : 'room 4',
       'position' : 'position4',
       'description' :""" """,
-      'exits' : {} 
+      'exits' : {'east': 'rm5'}
 }
 
 r5 = {'name' : 'room 5',
       'position' : 'position5',
       'description' :""" """,
-      'exits' : {'north' : 'rm7'} 
+      'exits' : {'north' : 'rm7'}
 }
 
 r6 = {'name' : 'room 6',
       'position' : 'position6',
       'description' :""" """,
-      'exits' : {'north' : 'rm8'} 
+      'exits' : {'north' : 'rm8'}
 }
 
 r7 = {'name' : 'room 7',
       'position' : 'position7',
       'description' :""" """,
-      'exits' : {'north' : 'bossrm', 'east' : 'rm8'} 
+      'exits' : {'north' : 'bossrm', 'east' : 'rm8'}
 }
 
 r8 = {'name' : 'room 8',
       'position' : 'position8',
       'description' :""" """,
-      'exits' : {'east' : 'rm9', 'west' : 'rm7'} 
+      'exits' : {'east' : 'rm9', 'west' : 'rm7'}
 }
 
 r9 = {'name' : 'room 9',
       'position' : 'position9',
       'description' :""" """,
-      'exits' : {} 
+      'exits' : {'west': 'rm8'}
 }
 
 boss = {'name' : 'boss room',
         'position' : 'position_boss',
       'description' :""" """,
-      'exits' : {} 
+      'exits' : {}
 }
 
 start = {'name' : 'start',
          'position' : 'position_start',
       'description' :""" """,
-      'exits' : {'north' : 'rm2'} 
+      'exits' : {'north' : 'rm2'}
 }
 
 rooms = {'rm1' : r1,
