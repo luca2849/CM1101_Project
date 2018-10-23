5f90486615f729e4803e980eef6833a1c43058be
Tyler <Tylercolejenkins22@gmail.com>
2018-10-23 15:49:43 +0100

    Added ascii art

--- monsters.py
+++ monsters.py
@@ -20,12 +20,45 @@ class monster:
 
 #Tier 1 Monsters
 zombeez = monster('Zombeez', '''These little fellas ain't here to make honey. They're here to chew gum and eat brains.
-And they're all out of gum.''', 1, [bee_stingers], '''''')
+And they're all out of gum.''', 1, [bee_stingers], '''
+ ,,     ,,     ,,
+  oo    _oo_   ,oo,
+ /==\   /==\   /==\
+(/==\) (/==\) (/==\)
+  \/     \/     \/ ''')
 computer_on_a_scooter = monster('Computer on a Scooter', '''Half computer, half absolute legend, the raddest dude on earth. He shreds the streets on his 2005 razor scooter with sparks.
-All the ladies want him, but none can have him. He’s too dedicated to the scooter life. ''', 1, [ddos_attack_script], '''''')
+All the ladies want him, but none can have him. He’s too dedicated to the scooter life. ''', 1, [ddos_attack_script], '''
+  _________
+    / ======= \
+   / __________\
+  | ___________ |
+  | | -       | |
+  | |         | |
+  | |_________| |________________________
+  \=____________/                         )
+  / """"""""""" \                       /
+ / ::::::::::::: \                  =D-'
+(_________________)''')
 tequila_sheila = monster('Tequila Shelia', '''After a rough few months going through a messy divorce Sheila turned to the
 tequila. It was the only thing that gave her love anymore. She is high key an alcoholic
-and should probably go see someone but instead she’s stuck in a dungeon''', 1, [broken_bottle], '''''')
+and should probably go see someone but instead she’s stuck in a dungeon''', 1, [broken_bottle], '''
+ /////////////\\\\
+(((((((((((((( \\\\
+))) ~~      ~~  (((
+((( (*)     (*) )))
+)))     <       (((
+((( '\______/`  )))
+)))\___________/(((
+       _) (_
+      / \_/ \
+     /(     )\
+    // )___( \\
+    \\(     )//
+     (       )
+      |  |  |
+       | | |
+       | | |
+      _|_|_|_ ''')
 crabs_with_abs = monster('Crabs with Abs', '''These bad boys have been hitting the gym over the summer. They’ve bulked up big time.
 They’ve munched their way through protein, smashed their way through sit ups and injected a metric tonne of steroids.''', 1, [workout_routine], '''
      /\
@@ -42,7 +75,26 @@ man_made_mermaid = monster('Man-Made Mermaid', '''Created in a lab off-shore far
 Following her escape she took refuge within this dungeon and swears to protect it from any intruders at all costs.''', 2, [trident], '''''')
 half_a_giraffe = monster('Half a Giraffe', '''He’s yellow, brown and not tall at all. He’s a half the size of a normal giraffe but it’s the size of his heart that matters.
 He was outcast by his tribe and then found peace with his height when he was taken in by a tribe of similarly sized big horn unicorns.''', 2, [giraffe_drumstick], '''''')
-big_horn_unicorn = monster ('Big Horn Unicorn', 'This bad boy has an absolute unit of a horn on his head. That’s it. His horn is just an absolute machine.', 2, [unicorn_spear], '''''')
+big_horn_unicorn = monster ('Big Horn Unicorn', 'This bad boy has an absolute unit of a horn on his head. That’s it. His horn is just an absolute machine.', 2, [unicorn_spear], '''
+             ,,))))))));,
+           __)))))))))))))),
+\|/       -\(((((''((((((((.
+-*-==//////((''  .     `)))))),
+/|\      ))| o    ;-.    '(((((                                  ,(,
+         ( `|    /  )    ;))))'                               ,_))^;(~
+            |   |   |   ,))((((_     _____------~~~-.        %,;(;(>';'~
+            o_);   ;    )))(((` ~---~  `::           \      %%~~)(v;(`('~
+                  ;    ''````         `:       `:::|\,__,%%    );`'; ~
+                 |   _                )     /      `:|`----'     `-'
+           ______/\/~    |                 /        /
+         /~;;.____/;;'  /          ___--,-(   `;;;/
+        / //  _;______;'------~~~~~    /;;/\    /
+       //  | |                        / ;   \;;,\
+      (<_  | ;                      /',/-----'  _>
+       \_| ||_                     //~;~~~~~~~~~
+           `\_|                   (,~~  
+                                   \~\
+                                    ~~ ''')
 deceased_priest = monster('Deceased Priest', 'He’s just a zombie but dressed as a priest. No one really knows how he got the outfit, he was a plumber before he died.', 2, [holy_hand_gernade], '''''')
 #Tier 3 Monsters
 one_tonne_skeleton = monster('One Tonne Skeleton', '''This absolute unit of a skeleton used to belong to World's Strongest Man Eddie Hall.
