dc7b27ac7fec74c70ed1964c57bc51280f26ee25
HarryBatchelor <batchelorh@cardiff.ac.uk>
2018-10-23 15:34:59 +0100

    changes

--- monsters.py
+++ monsters.py
@@ -41,64 +41,11 @@ man_made_mermaid = monster('Man-Made Mermaid', '''Created in a lab off-shore far
 Following her escape she took refuge within this dungeon and swears to protect it from any intruders at all costs.''', 2, [trident], '''''')
 half_a_giraffe = monster('Half a Giraffe', '''He’s yellow, brown and not tall at all. He’s a half the size of a normal giraffe but it’s the size of his heart that matters.
 He was outcast by his tribe and then found peace with his height when he was taken in by a tribe of similarly sized big horn unicorns.''', 2, [giraffe_drumstick], '''''')
-big_horn_unicorn = monster ('Big Horn Unicorn', 'This bad boy has an absolute unit of a horn on his head. That’s it. His horn is just an absolute machine.', 2, [unicorn_spear], '''
-
-             ,,))))))));,
-           __)))))))))))))),
-\|/       -\(((((''((((((((.
--*-===//////((''  .     `)))))),
-/|\      ))| o    ;-.    '(((((                                  ,(,
-         ( `|    /  )    ;))))'                               ,_))^;(~
-            |   |   |   ,))((((_     _____------~~~-.        %,;(;(>';'~
-            o_);   ;    )))(((` ~---~  `::           \      %%~~)(v;(`('~
-                  ;    ''````         `:       `:::|\,__,%%    );`'; ~
-                 |   _                )     /      `:|`----'     `-'
-           ______/\/~    |                 /        /
-         /~;;.____/;;'  /          ___--,-(   `;;;/
-        / //  _;______;'------~~~~~    /;;/\    /
-       //  | |                        / ;   \;;,\
-      (<_  | ;                      /',/-----'  _>
-       \_| ||_                     //~;~~~~~~~~~
-           `\_|                   (,~~  -Tua Xiong
-                                   \~\
-                                    ~~
-                                    ''')
+big_horn_unicorn = monster ('Big Horn Unicorn', 'This bad boy has an absolute unit of a horn on his head. That’s it. His horn is just an absolute machine.', 2, [unicorn_spear], '''''')
 deceased_priest = monster('Deceased Priest', 'He’s just a zombie but dressed as a priest. No one really knows how he got the outfit, he was a plumber before he died.', 2, [holy_hand_gernade], '''''')
 #Tier 3 Monsters
 one_tonne_skeleton = monster('One Tonne Skeleton', '''This absolute unit of a skeleton used to belong to World's Strongest Man Eddie Hall.
-He's retired now so he doesn't need it, now it's your problem.''', 3, [bone_saw], '''
-          .7
-            .'/
-           / /
-          / /
-         / /
-        / /
-       / /
-      / /
-     / /         
-    / /          
-  __|/
-,-\__\
-|f-"Y\|
-\()7L/
- cgD                            __ _
- |\(                          .'  Y '>,
-  \ \                        / _   _   \
-   \\\                       )(_) (_)(|}
-    \\\                      {  4A   } /
-     \\\                      \uLuJJ/\l
-      \\\                     |3    p)/
-       \\\___ __________      /nnm_n//
-       c7___-__,__-)\,__)(".  \_>-<_/D
-                  //V     \_"-._.__G G_c__.-__<"/ ( \
-                         <"-._>__-,G_.___)\   \7\
-                        ("-.__.| \"<.__.-" )   \ \
-                        |"-.__"\  |"-.__.-".\   \ \
-                        ("-.__"". \"-.__.-".|    \_\
-                        \"-.__""|!|"-.__.-".)     \ \
-                         "-.__""\_|"-.__.-"./      \ l
-                          ".__""">G>-.__.-">       .--,_
-                              ""  G ''')
+He's retired now so he doesn't need it, now it's your problem.''', 3, [bone_saw], '''''')
 tyrannosaurus_mex = monster('Tyrannosaurus Mex', '''A spicy Mexican dinosaur rocking a fiery sombrero, wielding some deadly maracas and busting some sick dance moves.
 Good luck with this big bad boy, you're going to need it.''', 3, [maracas], '''____
        ___                                      .-~. /_"-._
